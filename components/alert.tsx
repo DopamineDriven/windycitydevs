18230801a87774ee76aa3957d64310ed6178b3d6
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-21 21:19:54 -0500

    updates

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Container from "./container";
 import cn from "classnames";
-import { EXAMPLE_PATH } from "../lib/constants";
-import {
-	GithubOutlined,
-	TwitterOutlined,
-	FacebookOutlined,
-	GoogleOutlined
-} from "@ant-design/icons";
+// import { EXAMPLE_PATH } from "../lib/constants";
+// import {
+// 	GithubOutlined,
+// 	TwitterOutlined,
+// 	FacebookOutlined,
+// 	GoogleOutlined
+// } from "@ant-design/icons";
 
 type Props = {
 	preview?: boolean;
